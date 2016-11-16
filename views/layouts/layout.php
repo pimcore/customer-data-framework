280b33266ec381c8a88bbda5c3ca5dc459709998
Mathias Geat <mathias.geat@elements.at>
2016-11-16 13:38:30 +0100

    Update layout to reference built admin LTE files

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -4,31 +4,26 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
     <title>Customer Management</title>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <?php
+    $this->headLink()->appendStylesheet('/plugins/CustomerManagementFramework/static/dist/css/style.css');
+    ?>
 
-    <?=$this->headLink()?>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <?= $this->headLink(); ?>
 </head>
 <body>
 
-<?=$this->layout()->content?>
+<?= $this->layout()->content ?>
+<?= $this->jsConfig() ?>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<?=$this->jsConfig()?>
-<script src="/plugins/CustomerManagementFramework/static/js/script.js"></script>
+<?php
+$this->headScript()->appendFile('/plugins/CustomerManagementFramework/static/dist/js/lib.js');
+$this->headScript()->appendFile('/plugins/CustomerManagementFramework/static/dist/js/script.js');
+?>
+
+<?= $this->headScript() ?>
+
 </body>
 </html>
