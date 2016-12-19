1be25a8ed8bfab0c5a59721b19d949e856636335
Mathias Geat <mathias.geat@elements.at>
2016-12-19 15:55:54 +0100

    Add dummy export toolkit cluster interpreter

---
+++ lib/CustomerManagementFramework/ExportToolkit/ExportService/AttributeClusterInterpreter/Json.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace CustomerManagementFramework\ExportToolkit\ExportService\AttributeClusterInterpreter;
+
+use ExportToolkit\ExportService\AttributeClusterInterpreter\AbstractAttributeClusterInterpreter;
+use Pimcore\Model\Object\AbstractObject;
+
+class Json extends AbstractAttributeClusterInterpreter
+{
+    /**
+     * This method is executed before the export is launched.
+     * For example it can be used to clean up old export files, start a database transaction, etc.
+     * If not needed, just leave the method empty.
+     *
+     */
+    public function setUpExport()
+    {
+        // noop
+    }
+
+    /**
+     * This method is executed after all defined attributes of an object are exported.
+     * The to-export data is stored in the array $this->data[OBJECT_ID].
+     * For example it can be used to write each exported row to a destination database,
+     * write the exported entries to a file, etc.
+     * If not needed, just leave the method empty.
+     *
+     * @param AbstractObject $object
+     */
+    public function commitDataRow(AbstractObject $object)
+    {
+        dump($this->data[$object->getId()]);
+    }
+
+    /**
+     * This method is executed after all objects are exported.
+     * If not cleaned up in the commitDataRow-method, all exported data is stored in the array $this->data.
+     * For example it can be used to write all data to a xml file or commit a database transaction, etc.
+     *
+     */
+    public function commitData()
+    {
+        dump($this->data);
+    }
+
+    /**
+     * This method is executed of an object is not exported (anymore).
+     * For example it can be used to remove the entries from a destination database, etc.
+     *
+     * @param AbstractObject $object
+     */
+    public function deleteFromExport(AbstractObject $object)
+    {
+        // noop
+    }
+}
