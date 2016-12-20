5f73ad8897d0c35872497f2d486b881b7883ea6b
Mathias Geat <mathias@ailoo.net>
2016-12-20 15:01:43 +0100

    Add a export getter and interpreter handling fetching and formatting of dates (birthdate, specifically)

---
+++ lib/CustomerManagementFramework/Mailchimp/ExportToolkit/Interpreter/Date.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CustomerManagementFramework\Mailchimp\ExportToolkit\Interpreter;
+
+use ExportToolkit\ExportService\IInterpreter;
+
+/**
+ * Formats a DateTime with the given format
+ */
+class Date implements IInterpreter
+{
+    const DEFAULT_FORMAT = 'd/m';
+
+    /**
+     * @param $value
+     * @param null $config
+     * @return string
+     */
+    public static function interpret($value, $config = null)
+    {
+        if (!$value) {
+            return $value;
+        }
+
+        $config = (array)$config;
+        $format = (isset($config['format'])) ? $config['format'] : static::DEFAULT_FORMAT;
+
+        if ($value instanceof \DateTime) {
+            return $value->format($format);
+        }
+    }
+}
