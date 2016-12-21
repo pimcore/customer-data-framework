c947ce1c56916f1a1639784a783d1fb6805ddea4
Mathias Geat <mathias.geat@elements.at>
2016-12-21 15:57:19 +0100

    Update export service to handle export notes for every kind of object, not only customers

--- lib/CustomerManagementFramework/Mailchimp/ExportService.php
+++ lib/CustomerManagementFramework/Mailchimp/ExportService.php
@@ -3,7 +3,6 @@
 namespace CustomerManagementFramework\Mailchimp;
 
 use Carbon\Carbon;
-use CustomerManagementFramework\Model\CustomerInterface;
 use DrewM\MailChimp\MailChimp;
 use Pimcore\Model\Element\ElementInterface;
 use Pimcore\Model\Element\Note;
@@ -76,40 +75,28 @@ class ExportService
     }
 
     /**
-     * @param CustomerInterface $customer
-     * @return \DateTime|null
+     * @param ElementInterface $object
+     * @return bool
      */
-    public function getLastExportDateTime(CustomerInterface $customer)
+    public function wasExported(ElementInterface $object)
     {
-        $notes = $this->getExportNotes($customer);
-        if ($notes) {
-            return $this->getNoteDateTime($notes[0]);
-        }
+        return null !== $this->getLastExportDateTime($object);
     }
 
     /**
-     * @param CustomerInterface $customer
+     * @param ElementInterface|Concrete $object
      * @return bool
      */
-    public function wasCreated(CustomerInterface $customer)
-    {
-        return null !== $this->getLastExportDateTime($customer);
-    }
-
-    /**
-     * @param CustomerInterface|Concrete $customer
-     * @return bool
-     */
-    public function needsUpdate(CustomerInterface $customer)
+    public function needsUpdate(ElementInterface $object)
     {
         // no last export -> needs update
-        if (!$this->wasCreated($customer)) {
+        if (!$this->wasExported($object)) {
             return true;
         }
 
-        if ($customer->getModificationDate()) {
-            $lastExportDate   = $this->getLastExportDateTime($customer);
-            $modificationDate = Carbon::createFromTimestamp($customer->getModificationDate(), $lastExportDate->getTimezone());
+        if ($object->getModificationDate()) {
+            $lastExportDate   = $this->getLastExportDateTime($object);
+            $modificationDate = Carbon::createFromTimestamp($object->getModificationDate(), $lastExportDate->getTimezone());
 
             // item was modified after last export -> needs update
             if ($modificationDate > $lastExportDate) {
@@ -121,43 +108,74 @@ class ExportService
     }
 
     /**
-     * @param CustomerInterface|ElementInterface $customer
+     * @param ElementInterface $object
      * @param \DateTime|null $date
      * @return Note
      */
-    public function createExportNote(CustomerInterface $customer, \DateTime $date = null)
+    public function createExportNote(ElementInterface $object, $remoteId, \DateTime $date = null)
     {
         if (!$date) {
             $date = Carbon::now();
         }
 
         $note = new Note();
-        $note->setElement($customer);
+        $note->setElement($object);
         $note->setDate($date->getTimestamp());
         $note->setType(static::NOTE_TYPE);
+        $note->setDescription($this->listId);
         $note->setTitle('Mailchimp Export');
+        $note->addData('list_id', 'text', $this->listId);
+        $note->addData('mailchimp_id', 'text', $remoteId);
 
         return $note;
     }
 
     /**
-     * @param CustomerInterface $customer
-     * @return Note\Listing|\Pimcore\Model\Object\Listing\Dao|Note[]
+     * @param ElementInterface $object
+     * @param bool $refresh
+     * @return Note[]|Note\Listing|\Pimcore\Model\Object\Listing\Dao
      */
-    public function getExportNotes(CustomerInterface $customer, $refresh = false)
+    public function getExportNotes(ElementInterface $object, $refresh = false)
     {
-        if (!isset($this->notes[$customer->getId()]) || $refresh) {
+        if (!isset($this->notes[$object->getId()]) || $refresh) {
             /** @var Note\Listing|\Pimcore\Model\Object\Listing\Dao $list */
             $list = new Note\Listing();
             $list->setOrderKey('date');
             $list->setOrder('desc');
             $list->addConditionParam('type = ?', static::NOTE_TYPE);
-            $list->addConditionParam('cid = ?', $customer->getId());
+            $list->addConditionParam('description = ?', $this->listId);
+            $list->addConditionParam('cid = ?', $object->getId());
 
-            $this->notes[$customer->getId()] = $list->load();
+            $this->notes[$object->getId()] = $list->load();
         }
 
-        return $this->notes[$customer->getId()];
+        return $this->notes[$object->getId()];
+    }
+
+    /**
+     * @param ElementInterface $object
+     * @param bool $refresh
+     * @return Note|null
+     */
+    public function getLastExportNote(ElementInterface $object, $refresh = false)
+    {
+        $notes = $this->getExportNotes($object, $refresh);
+
+        if ($notes) {
+            return $notes[0];
+        }
+    }
+
+    /**
+     * @param ElementInterface $object
+     * @return \DateTime|null
+     */
+    public function getLastExportDateTime(ElementInterface $object)
+    {
+        $note = $this->getLastExportNote($object);
+        if ($note) {
+            return $this->getNoteDateTime($note);
+        }
     }
 
     /**
