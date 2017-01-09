8fd39004e87af041d1dd24ab365673d8a2ce17c8
Markus Moser <markus.moser@elements.at>
2017-01-09 11:46:22 +0100

    Add PHPdoc to SegmentManagerInterface

--- lib/CustomerManagementFramework/SegmentManager/SegmentManagerInterface.php
+++ lib/CustomerManagementFramework/SegmentManager/SegmentManagerInterface.php
@@ -22,6 +22,8 @@ interface SegmentManagerInterface {
     public function __construct(LoggerInterface $logger);
 
     /**
+     * Returns a list of customers which are within the given customer segments.
+     * 
      * @param int[] $segmentIds
      *
      * @return CustomerSegment\Listing
@@ -29,30 +31,38 @@ interface SegmentManagerInterface {
     public function getCustomersBySegmentIds(array $segmentIds, $conditionMode = self::CONDITION_AND);
 
     /**
+     * Returns the CustomerSegment with the given ID.
+     * 
      * @param int $id
      * @return CustomerSegmentInterface
      */
     public function getSegmentById($id);
 
     /**
+     * Returns the CustomerSegmentGroup with the given ID.
+     * 
      * @param int $id
      * @return CustomerSegmentGroup
      */
     public function getSegmentGroupById($id);
 
     /**
+     * Returns an array with all customer segments. Optionally this could be filtered by given params.
+     * 
      * @param array $params
      *
      * @return CustomerSegment[]
      */
-    public function getSegments(array $params);
+    public function getSegments(array $params = []);
 
     /**
+     * Returns an array with all customer segment groups. Optionally this could be filtered by given params.
+     * 
      * @param array $params
      *
      * @return CustomerSegment[]
      */
-    public function getSegmentGroups(array $params);
+    public function getSegmentGroups(array $params = []);
 
     /**
      * @return void
@@ -151,14 +161,20 @@ interface SegmentManagerInterface {
 
     /**
      * @param CustomerInterface $customer
+     * @param CustomerSegmentInterface $segment
+     * 
+     * @return bool
+     * /
+    public function customerHasSegment(CustomerInterface $customer, CustomerSegmentInterface $segment);
+
+    /**
+     * @param CustomerInterface $customer
      *
      * @return void
      */
     public function addCustomerToChangesQueue(CustomerInterface $customer);
 
     public function preSegmentUpdate(CustomerSegmentInterface $segment);
-
-    public function customerHasSegment(CustomerInterface $customer, CustomerSegmentInterface $segment);
 
 
 }
