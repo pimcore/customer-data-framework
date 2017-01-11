8345c3230697cd71766aac5d90e650a3040d74f1
markus-moser <markus.moser@elements.at>
2017-01-11 16:14:35 +0100

    Update activities PHPdoc

--- models/CustomerManagementFramework/Model/AbstractActivity.php
+++ models/CustomerManagementFramework/Model/AbstractActivity.php
@@ -19,55 +19,99 @@ abstract class AbstractActivity implements ActivityInterface {
 
     protected $customer;
 
+    /**
+     * @return bool
+     */
     public function cmfIsActive() {
         return true;
     }
 
+    /**
+     * @return Carbon
+     */
     public function cmfGetActivityDate()
     {
         return new Carbon();
     }
 
+    /**
+     * @return bool
+     */
     public function cmfUpdateOnSave()
     {
         return true;
     }
 
+    /**
+     * @return bool
+     */
     public function cmfWebserviceUpdateAllowed()
     {
         return !($this instanceof PersistentActivityInterface);
     }
 
+    /**
+     * @param array $data
+     *
+     * @return bool
+     */
     public function cmfUpdateData(array $data)
     {
         return false;
     }
 
+    /**
+     * @param array $data
+     * @param bool  $fromWebservice
+     *
+     * @return bool
+     */
     public static function cmfCreate(array $data, $fromWebservice = false)
     {
         return false;
     }
 
+    /**
+     * @param ActivityStoreEntryInterface $entry
+     *
+     * @return bool
+     */
     public static function cmfGetOverviewData(ActivityStoreEntryInterface $entry)
     {
         return false;
     }
 
+    /**
+     * @param ActivityStoreEntryInterface $entry
+     *
+     * @return array
+     */
     public static function cmfGetDetailviewData(ActivityStoreEntryInterface $entry)
     {
         return $entry->getAttributes();
     }
 
+    /**
+     * @param ActivityStoreEntryInterface $entry
+     *
+     * @return bool
+     */
     public static function cmfGetDetailviewTemplate(ActivityStoreEntryInterface $entry)
     {
         return false;
     }
 
+    /**
+     * @return CustomerInterface
+     */
     public function getCustomer()
     {
         return $this->customer;
     }
 
+    /**
+     * @param CustomerInterface $customer
+     */
     public function setCustomer($customer)
     {
         $this->customer = $customer;
