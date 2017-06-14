bdac6ecc8f8345fe73203473160a3344887bfa09
markus-moser <markus.moser@elements.at>
2017-06-14 12:49:11 +0200

    add decline duplicate javascript

--- frontend/js/functions.js
+++ frontend/js/functions.js
@@ -1,24 +1,3 @@
 ;(function ($) {
     'use strict';
-
-    app.DeclineDuplicates = (function () {
-        if($('.js-decline-duplicate').length) {
-            $('body').on('click', '.js-decline-duplicate', function() {
-                var $duplicateItem = $(this).closest('.js-duplicates-item');
-                $duplicateItem.css("opacity", 0.5);
-
-                $.ajax({
-                    url: '/plugin/CustomerManagementFramework/duplicates/decline-duplicate?id=' + $(this).data('id'),
-                    success: function(data) {
-                        if(data.success) {
-                            $duplicateItem.remove();
-                        } else {
-                            $duplicateItem.css("opacity", 1);
-                        }
-                    }
-
-                });
-            });
-        }
-    }());
 })(jQuery);
