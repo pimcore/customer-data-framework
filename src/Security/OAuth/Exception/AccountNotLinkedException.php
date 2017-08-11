132c1a294ed10637a45f09574426296a38c0324c
Mathias Geat <mathias.geat@elements.at>
2017-08-11 12:43:11 +0200

    Remove pimcore license headers added during symfony oauth implementation

--- src/Security/OAuth/Exception/AccountNotLinkedException.php
+++ src/Security/OAuth/Exception/AccountNotLinkedException.php
@@ -6,18 +6,6 @@ namespace CustomerManagementFrameworkBundle\Security\OAuth\Exception;
 
 use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
 
-/**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Enterprise License (PEL)
- * Full copyright and license information is available in
- * LICENSE.md which is distributed with this source code.
- *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PEL
- */
 class AccountNotLinkedException extends \HWI\Bundle\OAuthBundle\Security\Core\Exception\AccountNotLinkedException
 {
     public function getToken(): OAuthToken
