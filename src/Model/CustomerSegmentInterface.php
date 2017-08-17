5a7c416b7de7d82c03ab234dd031d693a125d5de
markus-moser <markus.moser@elements.at>
2017-08-17 12:29:10 +0200

    [CustomerSegmentInterface] add phpdoc types

--- src/Model/CustomerSegmentInterface.php
+++ src/Model/CustomerSegmentInterface.php
@@ -8,30 +8,72 @@
 
 namespace CustomerManagementFrameworkBundle\Model;
 
+use Pimcore\Model\Object\CustomerSegmentGroup;
+
 interface CustomerSegmentInterface
 {
 
+    /**
+     * @return int
+     */
     public function getId();
 
+    /**
+     * @return string
+     */
     public function getName();
 
+    /**
+     * @param $name
+     * @return string
+     */
     public function setName($name);
 
+    /**
+     * @return string
+     */
     public function getReference();
 
+    /**
+     * @param string $reference
+     * @return void
+     */
     public function setReference($reference);
 
+    /**
+     * @return CustomerSegmentGroup
+     */
     public function getGroup();
 
+    /**
+     * @param CustomerSegmentGroup $group
+     * @return void
+     */
     public function setGroup($group);
 
+    /**
+     * @return bool
+     */
     public function getCalculated();
 
+    /**
+     * @param bool $calculated
+     * @return void
+     */
     public function setCalculated($calculated);
 
+    /**
+     * @return []
+     */
     public function getDataForWebserviceExport();
 
+    /**
+     * @return void
+     */
     public function save();
 
+    /**
+     * @return void
+     */
     public function delete();
 }
