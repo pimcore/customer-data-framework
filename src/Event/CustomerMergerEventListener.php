e601a9eed41dfdb419f759d94414871d7090e52f
markus-moser <markus.moser@elements.at>
2017-08-24 18:55:30 +0200

    Make customer merging via object merger work again

---
+++ src/Event/CustomerMergerEventListener.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Pimcore Customer Management Framework Bundle
+ * Full copyright and license information is available in
+ * License.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (C) Elements.at New Media Solutions GmbH
+ * @license    GPLv3
+ */
+
+namespace CustomerManagementFrameworkBundle\Event;
+
+class CustomerMergerEventListener
+{
+
+    public function onPostMerge(\Symfony\Component\EventDispatcher\GenericEvent $e)
+    {
+        $sourceCustomer = \Pimcore::getContainer()->get('cmf.customer_provider')->getById($e->getArgument('sourceId'));
+        $targetCustomer = \Pimcore::getContainer()->get('cmf.customer_provider')->getById($e->getArgument('targetId'));
+
+        if($sourceCustomer && $targetCustomer) {
+            \Pimcore::getContainer()->get('cmf.customer_merger')->mergeCustomers($sourceCustomer, $targetCustomer, false);
+        }
+    }
+
+}
