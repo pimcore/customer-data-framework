b21a8df98dc727c673ec300e699f8ac75e387f9a
Christian Fasching <christian.fasching@elements.at>
2017-09-26 13:09:48 +0200

    moved segment assignment to last position and added translations for title

--- src/Resources/public/js/startup.js
+++ src/Resources/public/js/startup.js
@@ -267,8 +267,9 @@ pimcore.plugin.customermanagementframework = Class.create(pimcore.plugin.admin, 
         }
 
         this.segmentTab = new pimcore.plugin.customermanagementframework.segmentAssignmentTab(element, type);
-        element.tab.items.items[1].insert(1, this.segmentTab.getLayout());
-        element.tab.items.items[1].updateLayout();
+        var tabPanel = element.tab.items.items[1];
+        tabPanel.insert(tabPanel.items.length, this.segmentTab.getLayout());
+        tabPanel.updateLayout();
     },
 
     preSaveDocument: function (element, type, task, only) {
