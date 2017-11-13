76188625ce5d9131822688df38838c3305c9309f
Mathias Geat <mathias.geat@elements.at>
2017-11-13 11:39:19 +0100

    [TARGETING] Rename "SegmentTracked" to "HasSegment" and handle customer and tracked segments

--- src/Resources/public/js/pimcore/targeting/conditions.js
+++ src/Resources/public/js/pimcore/targeting/conditions.js
@@ -2,14 +2,22 @@
     'use strict';
 
     pimcore.settings.targeting.conditions.register(
-        'cmf_segment_tracked',
+        'cmf_has_segment',
         Class.create(pimcore.settings.targeting.condition.abstract, {
             getName: function () {
-                return t("plugin_cmf_targeting_condition_segment_tracked");
+                return t("plugin_cmf_targeting_condition_has_segment");
             },
 
             getPanel: function (panel, data) {
                 var id = Ext.id();
+
+                var getDataValue = function(key, defaultValue) {
+                    if ('undefined' === typeof data[key]) {
+                        return defaultValue;
+                    }
+
+                    return data[key];
+                };
 
                 return new Ext.form.FormPanel({
                     id: id,
@@ -34,10 +42,22 @@
                             emptyText: t("plugin_cmf_select_a_segment")
                         },
                         {
+                            name: "considerTrackedSegments",
+                            fieldLabel: t("plugin_cmf_consider_tracked_segments"),
+                            xtype: "checkbox",
+                            checked: getDataValue('considerTrackedSegments', true)
+                        },
+                        {
+                            name: "considerCustomerSegments",
+                            fieldLabel: t("plugin_cmf_consider_customer_segments"),
+                            xtype: "checkbox",
+                            checked: getDataValue('considerCustomerSegments', true)
+                        },
+                        {
                             xtype: 'numberfield',
                             fieldLabel: t("threshold"),
                             name: "threshold",
-                            value: data.threshold || 1,
+                            value: getDataValue('threshold', 1),
                             width: 200,
                             minValue: 1,
                             allowDecimals: false
@@ -45,7 +65,7 @@
                         {
                             xtype: "hidden",
                             name: "type",
-                            value: "cmf_segment_tracked"
+                            value: "cmf_has_segment"
                         }
                     ]
                 });
---
+++ src/Targeting/Condition/HasSegment.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace CustomerManagementFrameworkBundle\Targeting\Condition;
+
+use CustomerManagementFrameworkBundle\Targeting\DataProvider\CustomerSegments;
+use CustomerManagementFrameworkBundle\Targeting\SegmentTracker;
+use Pimcore\Targeting\Condition\DataProviderDependentConditionInterface;
+use Pimcore\Targeting\DataProvider\Session;
+use Pimcore\Targeting\Model\VisitorInfo;
+use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HasSegment implements DataProviderDependentConditionInterface
+{
+    /**
+     * @var int|null
+     */
+    private $segmentId;
+
+    /**
+     * @var array
+     */
+    private $options = [];
+
+    /**
+     * @param int|null $segmentId
+     * @param array $options
+     */
+    public function __construct(int $segmentId = null, array $options = [])
+    {
+        $this->segmentId = $segmentId;
+
+        $resolver = new OptionsResolver();
+        $this->configureOptions($resolver);
+
+        $this->options = $resolver->resolve($options);
+    }
+
+    private function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'threshold'                => 1,
+            'considerCustomerSegments' => true,
+            'considerTrackedSegments'  => true
+        ]);
+
+        $resolver->setAllowedTypes('threshold', 'int');
+        $resolver->setAllowedValues('threshold', function ($value) {
+            return $value > 0;
+        });
+
+        $resolver->setAllowedTypes('considerCustomerSegments', 'bool');
+        $resolver->setAllowedTypes('considerTrackedSegments', 'bool');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function fromConfig(array $config)
+    {
+        return new self(
+            $config['segmentId'],
+            [
+                'threshold'                => $config['threshold'] ?? 1,
+                'considerCustomerSegments' => $config['considerCustomerSegments'] ?? true,
+                'considerTrackedSegments'  => $config['considerTrackedSegments'] ?? true
+            ]
+        );
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDataProviderKeys(): array
+    {
+        $providers = [];
+
+        if ($this->options['considerCustomerSegments']) {
+            $providers[] = CustomerSegments::PROVIDER_KEY;
+        }
+
+        if ($this->options['considerTrackedSegments']) {
+            $providers[] = Session::PROVIDER_KEY;
+        }
+
+        return $providers;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function canMatch(): bool
+    {
+        return null !== $this->segmentId;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function match(VisitorInfo $visitorInfo): bool
+    {
+        if ($this->options['considerCustomerSegments'] && $this->matchCustomerSegments($visitorInfo)) {
+            return true;
+        }
+
+        if ($this->options['considerTrackedSegments'] && $this->matchTrackedSegments($visitorInfo)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function matchCustomerSegments(VisitorInfo $visitorInfo): bool
+    {
+        $segments = $visitorInfo->get(CustomerSegments::PROVIDER_KEY);
+
+        return $this->matchSegments($segments);
+    }
+
+    private function matchTrackedSegments(VisitorInfo $visitorInfo): bool
+    {
+        $bag = $visitorInfo->get(Session::PROVIDER_KEY);
+        if (!($bag && $bag instanceof NamespacedAttributeBag)) {
+            return false;
+        }
+
+        $segments = $bag->get(SegmentTracker::KEY_SEGMENTS, []);
+
+        return $this->matchSegments($segments);
+    }
+
+    private function matchSegments(array $segments): bool
+    {
+        if (isset($segments[$this->segmentId])) {
+            return $segments[$this->segmentId] >= $this->options['threshold'];
+        }
+
+        return false;
+    }
+}
