08e6ffd16e381c44c613c67a82d7c4a2b450e616
Christian Fasching <fashxp@users.noreply.github.com>
2019-10-01 13:57:05 +0200

    license update

--- src/Newsletter/Note/NoteHandler.php
+++ src/Newsletter/Note/NoteHandler.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: tmittendorfer
- * Date: 18.07.2018
- * Time: 10:03
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
 use Pimcore\Model\Element\ElementInterface;
