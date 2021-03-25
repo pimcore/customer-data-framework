7a3dd9bf30601403495508464604529fafef404c
Christian Fasching <christian.fasching@pimcore.com>
2021-03-25 21:43:08 +0100

    github actions test

---
+++ .github/ci/scripts/setup-pimcore-6-9-environment.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+mkdir -p var/config
+mkdir -p bin
+
+cp -r .github/ci/files/app app
+cp -r .github/ci/files/bin/console bin/console
+chmod 755 bin/console
+cp -r .github/ci/files/web web
+
+cp .github/ci/files/extensions.template.php var/config/extensions.php
+cp app/config/parameters.example.yml app/config/parameters.yml
+
+# temp. until elasticsearch/elasticsearch 7.11 is released
+composer config minimum-stability "dev"
+composer config prefer-stable true
+composer require pimcore/pimcore:6.9.x-dev --no-update
+
+# move that to composer.json when only pimcore X
+#composer require codeception/module-symfony:^1.6.0 --no-update
+#composer require codeception/phpunit-wrapper:^9 --no-update
+#composer require codeception/module-asserts --no-update
