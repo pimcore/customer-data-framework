af2c6ec0b1ac6ed9f9854fc42b907c81398517ec
Christian Fasching <christian.fasching@pimcore.com>
2021-03-25 15:45:03 +0100

    github actions test

--- .github/ci/pimcore_x/scripts/setup-pimcore-environment.sh
+++ .github/ci/pimcore_x/scripts/setup-pimcore-environment.sh
@@ -4,13 +4,13 @@ set -eu
 
 mkdir -p var/config
 
-cp -r .github/ci/files/config/. config
-cp -r .github/ci/files/templates templates
-cp -r .github/ci/files/bin/console bin/console
-cp -r .github/ci/files/src src
-cp -r .github/ci/files/public public
-cp .github/ci/files/extensions.template.php var/config/extensions.php
-cp .github/ci/files/.env ./
+cp -r .github/ci/pimcore_x/files/config/. config
+cp -r .github/ci/pimcore_x/files/templates templates
+cp -r .github/ci/pimcore_x/files/bin/console bin/console
+cp -r .github/ci/pimcore_x/files/src src
+cp -r .github/ci/pimcore_x/files/public public
+cp .github/ci/pimcore_x/files/extensions.template.php var/config/extensions.php
+cp .github/ci/pimcore_x/files/.env ./
 
 # temp. until elasticsearch/elasticsearch 7.11 is released
 composer config minimum-stability "dev"
