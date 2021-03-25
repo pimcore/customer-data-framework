99add3b5d33a3f76374537966fa4da80919944a9
Christian Fasching <christian.fasching@pimcore.com>
2021-03-25 16:15:15 +0100

    github actions test

--- .github/ci/scripts/setup-pimcore-x-environment.sh
+++ .github/ci/scripts/setup-pimcore-x-environment.sh
@@ -17,3 +17,7 @@ cp .github/ci/pimcore_x/files/.env ./
 composer config minimum-stability "dev"
 composer config prefer-stable true
 composer require pimcore/pimcore:dev-master --no-update
+
+# move that to composer.json when only pimcore X
+composer require codeception/module-symfony:^1.6.0 --no-update
+composer require codeception/phpunit-wrapper:^9 --no-update
