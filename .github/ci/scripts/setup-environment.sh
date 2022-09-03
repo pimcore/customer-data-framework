c8cbf7213bf1a333a6f7a4506d722e391eff8c0f
Sebastian Blank <sebastian.bl@gmx.de>
2022-09-03 13:08:38 +0200

    Enable 11.x-dev tests again

--- .github/ci/scripts/setup-environment.sh
+++ .github/ci/scripts/setup-environment.sh
@@ -13,6 +13,6 @@ mkdir var/config
 cp .github/ci/files/extensions.template.php var/config/extensions.php
 cp .github/ci/files/.env ./
 
-: 'if [ ${DEPENDENCIES:-lowest} = "highest" ]; then
+if [ ${DEPENDENCIES:-lowest} = "highest" ]; then
     composer require pimcore/pimcore:"11.x-dev as 10.99.99" --no-update
-fi'
+fi
