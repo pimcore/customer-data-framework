9d9a31c6fa1ed12bc362063268baa52a14273e5a
Ashan Ghimire <ashanghimire10@gmail.com>
2022-09-14 16:01:07 +0545

    Fixed the comment message

--- src/Event/PimcoreObjectEventListener.php
+++ src/Event/PimcoreObjectEventListener.php
@@ -63,7 +63,7 @@ class PimcoreObjectEventListener
 
     public function onPostUpdate(ElementEventInterface $e)
     {
-        //do not update index when auto save or only saving version or when $e not DataObjectEvent
+        // Do not process the event any further in either cases of autoSave, save version only or the event not being an instance of DataObject
         if (($e->hasArgument('isAutoSave') && $e->getArgument('isAutoSave')) ||
             ($e->hasArgument('saveVersionOnly') && $e->getArgument('saveVersionOnly')) ||
             !$e instanceof DataObjectEvent) {
