c6dba58be5c462d210d6e755a7b4225883959d7b
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-10-19 18:39:36 +0200

    Follow up to #348

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,8 +1,6 @@
 <?php
 
 return [
-    //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
     Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle::class => ['all' => false],
     CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
 ];
