99cef87c9bc2c2fad17e876914f8b9dd30137479
Aashan Ghimire <ashanghimire10@gmail.com>
2022-11-10 00:26:49 +0545

    Added missing import statement

--- src/Security/Authentication/LoginManager.php
+++ src/Security/Authentication/LoginManager.php
@@ -26,6 +26,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Session\SessionAuthenticationStrategyInterface;
 
