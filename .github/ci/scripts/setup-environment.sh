bfd0fd9cdc7c1004e60f35d41517666bbc9c6881
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-12-21 16:27:09 +0100

    Add tests init script

--- .github/ci/scripts/setup-environment.sh
+++ .github/ci/scripts/setup-environment.sh
@@ -2,11 +2,13 @@
 
 set -eu
 
-cp -r .github/ci/files/config config
-cp -r .github/ci/files/templates templates
-cp -r .github/ci/files/bin bin
-cp -r .github/ci/files/kernel kernel
-cp -r .github/ci/files/public public
-cp -r .github/ci/files/var var
+mkdir -p var/config
+mkdir -p bin
 
-cp .github/ci/files/.env ./
+cp .github/ci/files/.env .
+cp -r .github/ci/files/config/. config
+cp -r .github/ci/files/templates/. templates
+cp -r .github/ci/files/bin/console bin/console
+chmod 755 bin/console
+cp -r .github/ci/files/kernel/. kernel
+cp -r .github/ci/files/public/. public
