bc7059b701f33031e0a9830260382090861e0c14
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-12-22 16:31:41 +0100

    fix setup-environment.sh

--- .github/ci/scripts/setup-environment.sh
+++ .github/ci/scripts/setup-environment.sh
@@ -2,13 +2,12 @@
 
 set -eu
 
-mkdir -p var/config
-mkdir -p bin
-
-cp .github/ci/files/.env .
-cp -r .github/ci/files/config/. config
-cp -r .github/ci/files/templates/. templates
-cp -r .github/ci/files/bin/console bin/console
+cp -r .github/ci/files/config config
+cp -r .github/ci/files/templates templates
+cp -r .github/ci/files/bin bin
 chmod 755 bin/console
-cp -r .github/ci/files/kernel/. kernel
-cp -r .github/ci/files/public/. public
+cp -r .github/ci/files/kernel kernel
+cp -r .github/ci/files/public public
+cp -r .github/ci/files/var var
+
+cp .github/ci/files/.env ./
