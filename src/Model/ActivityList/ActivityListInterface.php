d0d20f591d2f8625e3a6732b33bbcf041c10abbd
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-12-22 16:46:00 +0100

    fix version check

--- src/Model/ActivityList/ActivityListInterface.php
+++ src/Model/ActivityList/ActivityListInterface.php
@@ -20,7 +20,7 @@ use Pimcore\Version;
 
 //BC layer to support Pimcore 10
 //@TODO Change if condition to: version_compare(Version::getVersion(), '11.0', '>=')
-if (str_starts_with(Version::getVersion(), '11.')) {
+if (str_starts_with(Version::getVersion(), '11.') || str_starts_with(Version::getVersion(), 'v11.')) {
     interface ActivityListInterface extends PaginateListingInterface
     {
         public function setCondition(string $condition, float | array | bool | int | string $conditionVariables = null): static;
