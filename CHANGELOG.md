dd72fb912c54e9c91bc7f58a7f0e6f8680778091
JiaJia Ji <kingjia90@gmail.com>
2023-01-04 14:33:14 +0100

    [Improvement]: Add primary key to `plugin_cmf_deletions` table (#389)
    * Add primary key to plugin_cmf_deletions table (#252)
    
    * Add primary key to plugin_cmf_deletions
    
    * Add migration
    
    * Add migration for 6.9
    
    * disable foreign key checks
    
    * Task: add primary key to cmf_deletions
    
    * Task: add changelog
    
    Co-authored-by: Sebastian Blank <blank@data-factory.net>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
+#### v4.0.0
+ - Added primary key to `plugin_cmf_deletions` table.
 #### v3.3.0
 - Deprecated `DefaultCustomerProvider::getParentParentPath()`. Use  `getParentPath()` instead.
---
+++ src/Migrations/PimcoreX/Version20221229215900.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace CustomerManagementFrameworkBundle\Migrations\PimcoreX;
+
+use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Migrations\BundleAwareMigration;
+
+class Version20221229215900 extends BundleAwareMigration
+{
+    protected function getBundleName(): string
+    {
+        return 'PimcoreCustomerManagementFrameworkBundle';
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema): void
+    {
+        $deletionsTable = $schema->getTable('plugin_cmf_deletions');
+
+        if ($deletionsTable->getPrimaryKey()) {
+            $deletionsTable->dropPrimaryKey();
+        }
+
+        $this->addSql('SET foreign_key_checks = 0');
+        $this->addSql('ALTER TABLE `plugin_cmf_deletions` ADD PRIMARY KEY (`id`, `entityType`, `type`);');
+        $this->addSql('SET foreign_key_checks = 1');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema): void
+    {
+        $deletionsTable = $schema->getTable('plugin_cmf_deletions');
+
+        if ($deletionsTable->getPrimaryKey()) {
+            $deletionsTable->dropPrimaryKey();
+        }
+    }
+}
