55ae8f016a89d0a6fe1ec97147e1c8acac3daefa
Sebastian Blank <blank@data-factory.net>
2023-02-27 16:15:40 +0100

    Add PimcoreSeoBundle to fix codeception test

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -2,5 +2,6 @@
 
 return [
     Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle::class => ['all' => false],
+    Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
     CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
 ];
