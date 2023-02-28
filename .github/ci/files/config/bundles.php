433e0ab8087f1aa0dfd45d0e22c217988ede1879
Sebastian Blank <blank@data-factory.net>
2023-02-28 13:25:25 +0100

    Remove PimcoreSeoBundle

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle::class => ['all' => false],
-    Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
     CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
 ];
