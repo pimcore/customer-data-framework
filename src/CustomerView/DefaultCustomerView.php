c0548a113ded95b34e1a4d4a806c067f9878cf11
aryaantony92 <aryaantony92@users.noreply.github.com>
2023-05-15 14:22:18 +0000

    Apply php-cs-fixer changes

--- src/CustomerView/DefaultCustomerView.php
+++ src/CustomerView/DefaultCustomerView.php
@@ -110,7 +110,6 @@ class DefaultCustomerView implements CustomerViewInterface
         $vf = $this->viewFormatter;
 
         foreach ($definition->getFieldDefinitions() as $fd) {
-
             if ($fd->getInvisible() || $fd->getName() == 'password') {
                 continue;
             }
