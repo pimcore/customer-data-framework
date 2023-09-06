bce18c719359502ed1db777e0e3e388bca42af22
Refaat Al Ktifan <refaat.alktifan@diepartments.de>
2023-09-06 16:02:52 +0200

    fix create query builder on where condition for activity store

--- src/ActivityStore/MariaDb.php
+++ src/ActivityStore/MariaDb.php
@@ -283,7 +283,8 @@ class MariaDb extends SqlActivityStore implements ActivityStoreInterface
             ->addOrderBy('activityDate', 'asc');
 
         if ($ts = $params->getModifiedSinceTimestamp()) {
-            $select->where('modificationDate >= ?', $ts);
+            $select->where('modificationDate >= ?');
+            $select->setParameters([$ts]);
         }
 
         $paginator = $this->paginator->paginate($select, $page, $pageSize);
