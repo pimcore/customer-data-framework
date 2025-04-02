1bfdd2520dda173de10ff2481da6e4879d398beb
Alex Zamponi <562324+alexz707@users.noreply.github.com>
2025-04-02 14:45:34 +0200

    remove creation of table

--- tests/_support/Helper/Model.php
+++ tests/_support/Helper/Model.php
@@ -34,19 +34,6 @@ class Model extends AbstractDefinitionHelper
 
         $this->debug('[CMF] Running cmf installer');
 
-        //create migrations table in order to allow installation - needed for SettingsStoreAware Installer
-        \Pimcore\Db::get()->executeQuery('
-create table migration_versions
-(
-	version varchar(1024) not null
-		primary key,
-	executed_at datetime null,
-	execution_time int null
-)
-collate=utf8_unicode_ci;
-
-');
-
         // install ecommerce framework
         $installer = $pimcoreModule->getContainer()->get(Installer::class);
         $installer->install();
