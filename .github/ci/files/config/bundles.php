666eb2e963c5a741f6b088f3718dcd7b8f7a8b38
mcop1 <89011527+mcop1@users.noreply.github.com>
2025-05-27 14:16:22 +0200

    Update bundles.php

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'Pimcore\\Bundle\\AdminBundle\\PimcoreAdminBundle' => ['all' => true],
-    'Pimcore\\Bundle\\SeoBundle\\PimcoreSeoBundle' => ['all' => true],
+    Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle::class => ['all' => false],
+    CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
+    Pimcore\Bundle\AdminBundle\PimcoreAdminBundle::class => ['all' => true],
+    Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
 ];
