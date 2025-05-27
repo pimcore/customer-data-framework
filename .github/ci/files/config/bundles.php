c794fe2bd9cd2a9ff99ff90458b82e206b81ca3a
Bernhard Rusch <brusch@users.noreply.github.com>
2025-05-27 14:42:01 +0200

    🔄 synced file(s) with pimcore/sync-common-files (#587)
    * 🔄 synced file(s) with pimcore/sync-common-files
    
    * Update bundles.php
    
    * Update phpstan-baseline.neon
    
    * Update phpstan-baseline.neon
    
    * Update phpstan-baseline.neon
    
    * Update phpstan-baseline.neon
    
    ---------
    
    Co-authored-by: mcop1 <89011527+mcop1@users.noreply.github.com>

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle::class => ['all' => false],
     CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
     Pimcore\Bundle\AdminBundle\PimcoreAdminBundle::class => ['all' => true],
+    Pimcore\Bundle\SeoBundle\PimcoreSeoBundle::class => ['all' => true],
 ];
